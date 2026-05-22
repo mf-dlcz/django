1b8685a05760eccf1aa2de853ad731b6ef9bcf7b
Maria Delacruz <mar23iaa@gmail.com>
2026-05-21 19:38:20 -0700

    Updated models.py

--- django/pets_app/models.py
+++ django/pets_app/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+class Breed(models.Model):
+    name = models.CharField(max_length=100)
+    weight = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    height=models.DecimalField(max_digits=8, decimal_places=2, null=True)
+
+    def __str__(self):
+        return self.name + "-" + str(self.weight) + "-" + str(self.height)
+    
+
+class VaccinationCard(models.Model):
+    rabies = models.DateField(default=datetime.today().strftime('%Y-%m-%d'), null=False, blank=False)
+    hepatitis = models.DateField(null=True, blank = True)
+    borrelia = models.DateField(null=True, blank = True)
+    distemper = models.DateField(null=True, blank = True)
+
+    def __str__(self):
+        return str(self.id) + "-" + str(self.rabies)
+    
+
+class Gender(models.TextChoices):
+    FEMALE = "F", _("Female")
+    MALE = "M", _("Male")
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=6, choices=Gender.choices, default=Gender.FEMALE)
+    birth = models.DateField(default=None, null=True, blank=True)
+    owner = models.CharField(max_length=100)
+    weight = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    height = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    card = models.OneToOneField(VaccinationCard, on_delete=models.CASCADE)
+    breed = models.ForeignKey(Breed, blank=True)
+
+    def __str__(self):
+        return self.name + "-" + self.gender + "-" + str(self.birth) + "-" + self.owner
+    
+
+class VetVisit(models.Model):
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    vet = models.CharField(max_length=100)
+    date = models.DateField(default=datetime.today().strftime('%Y-%m-%d')
+    notes = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+    return self.pet.name + "-" + self.vet + "-" + str(self.date) + "-" + self.notes
